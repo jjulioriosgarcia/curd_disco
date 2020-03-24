0604160a68da7039e3851106a337875455d659b3
jjulioriosgarcia <62607858+jjulioriosgarcia@users.noreply.github.com>
2020-03-24 15:50:33 +0100

    Add files via upload

---
+++ DiscoRepository.java
@@ -0,0 +1,9 @@
+package com.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.crud.modelo.Disco;
+
+public interface DiscoRepository extends JpaRepository<Disco, Long> {
+
+}
