0b6417acf8236f2572aca1c7fccf8b686c741828
jjulioriosgarcia <62607858+jjulioriosgarcia@users.noreply.github.com>
2020-03-24 17:58:46 +0100

    Delete DiscoRepository.java

--- DiscoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crud.modelo.Disco;
-
-public interface DiscoRepository extends JpaRepository<Disco, Long> {
-
-}
